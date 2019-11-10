f22011c81b53c65a2eae6b4ca33fdf4fc2ccdb25
Edoardo Luppi <lp.edoardo@gmail.com>
2019-11-10 20:35:19 +0100

    refactor: align naming for UI elements

--- src/main/kotlin/com/github/lppedd/highlighter/javascript/JavaScriptReturnLineMarkerProvider.kt
+++ src/main/kotlin/com/github/lppedd/highlighter/javascript/JavaScriptReturnLineMarkerProvider.kt
@@ -9,7 +9,7 @@ import com.github.lppedd.highlighter.AbstractReturnLineMarkerProvider as ARLMP
 class JavaScriptReturnLineMarkerProvider : ARLMP<JSReturnStatement>(JSReturnStatement::class.java) {
   private val config = JavaScriptReturnHighlighterConfig.INSTANCE
 
-  override fun getName() = "JavaScript/TypeScript"
+  override fun getName() = "JavaScript & TypeScript"
   override fun isValidContext(psiElement: JSReturnStatement) =
       config.getHighlightStrategy().isValidContext(psiElement)
 }
