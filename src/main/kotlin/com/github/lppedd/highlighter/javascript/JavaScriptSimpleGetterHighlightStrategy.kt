22624ab087b46e4df60ab9cfe40f5366fb9cfb80
Edoardo Luppi <lp.edoardo@gmail.com>
2019-11-14 22:43:38 +0100

    refactor: improve simple getters identification for JavaScript

--- src/main/kotlin/com/github/lppedd/highlighter/javascript/JavaScriptSimpleGetterHighlightStrategy.kt
+++ src/main/kotlin/com/github/lppedd/highlighter/javascript/JavaScriptSimpleGetterHighlightStrategy.kt
@@ -1,15 +1,12 @@
 package com.github.lppedd.highlighter.javascript
 
 import com.github.lppedd.highlighter.ReturnHighlightStrategy
-import com.github.lppedd.highlighter.ReturnHighlightStrategy.PsiResult
-import com.github.lppedd.highlighter.ReturnHighlightStrategy.PsiResult.INVALID
-import com.github.lppedd.highlighter.ReturnHighlightStrategy.PsiResult.VALID
 import com.github.lppedd.highlighter.isChildOf
-import com.intellij.lang.javascript.psi.JSBlockStatement
-import com.intellij.lang.javascript.psi.JSEmptyStatement
-import com.intellij.lang.javascript.psi.JSFunction
-import com.intellij.lang.javascript.psi.JSReturnStatement
+import com.intellij.lang.javascript.psi.*
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.util.PsiTreeUtil
 
 /**
  * @author Edoardo Luppi
@@ -22,27 +19,54 @@ class JavaScriptSimpleGetterHighlightStrategy(
       return false
     }
 
-    val blockStatement = psiElement.isChildOf(JSBlockStatement::class.java)
+    val jsBlockStatement = psiElement.isChildOf(JSBlockStatement::class.java)
 
-    return if (blockStatement is JSBlockStatement && blockStatement.parent is JSFunction) {
-      checkJSBlockStatement(blockStatement) == VALID
+    return if (jsBlockStatement is JSBlockStatement && jsBlockStatement.parent is JSFunction) {
+      checkJSBlockStatement(jsBlockStatement, psiElement)
     } else {
       true
     }
   }
 
-  private fun checkJSBlockStatement(jsBlockStatement: JSBlockStatement): PsiResult {
-    for (value in jsBlockStatement.children) {
-      if ((value == jsBlockStatement.firstChild)
-          || value is JSEmptyStatement
-          || value is PsiWhiteSpace) {
-        continue
-      }
-
-      if (value is JSReturnStatement) return INVALID
-      if (value !is JSReturnStatement) return VALID
+  private fun checkJSBlockStatement(
+      jsBlockStatement: JSBlockStatement,
+      jsReturnStatement: JSReturnStatement
+  ): Boolean {
+    // To be considered a simple getter, only a return statement must be present,
+    // and that return statement must contain a simple expression (a reference or a literal)
+    // Valid examples are:
+    //    return a;
+    //    return a.b;
+    //    return a.b.c.d.e.f.length;
+    val innerStatementsCount = jsBlockStatement.children.count {
+      it !is LeafPsiElement &&
+      it !is JSEmptyStatement &&
+      it !is PsiWhiteSpace
     }
 
-    return VALID
+    if (innerStatementsCount != 1) {
+      // There are other statements in addition to the return one
+      return true
+    }
+
+    val nonEmptyExpressions = jsReturnStatement.children.filter {
+      it !is JSEmptyStatement && it !is PsiWhiteSpace
+    }
+
+    return when {
+      nonEmptyExpressions.isEmpty() -> false
+      nonEmptyExpressions.size == 1 -> {
+        val expression = nonEmptyExpressions[0]
+        when {
+          expression !is JSLiteralExpression &&
+          expression !is JSQualifiedExpression -> true
+          else -> !containsOnlyReferences(expression)
+        }
+      }
+      else -> true
+    }
   }
+
+  private fun containsOnlyReferences(psiElement: PsiElement) =
+    PsiTreeUtil.findChildOfAnyType(psiElement, JSCallExpression::class.java) == null
 }
