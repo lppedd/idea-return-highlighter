28545e1ffc489521458fe8c8c2801bc2f1985b8a
Edoardo Luppi <lp.edoardo@gmail.com>
2019-11-15 16:24:20 +0100

    build: add script to fix the author for all commits

---
+++ fix-author.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# git clone --bare https://github.com/user/repo.git
+# cd repo.git
+# -
+# git push --force --tags origin 'refs/heads/*'
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Your Correct Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
