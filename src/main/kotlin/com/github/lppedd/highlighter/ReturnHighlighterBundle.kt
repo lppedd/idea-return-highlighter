bac15dc63a862c4b37990aa3a64a5ec3907f175b
Edoardo Luppi <lp.edoardo@gmail.com>
2023-03-18 15:31:16 +0100

    refactor: replace deprecated CommonBundle with BundleBase

--- src/main/kotlin/com/github/lppedd/highlighter/ReturnHighlighterBundle.kt
+++ src/main/kotlin/com/github/lppedd/highlighter/ReturnHighlighterBundle.kt
@@ -1,6 +1,6 @@
 package com.github.lppedd.highlighter
 
-import com.intellij.CommonBundle
+import com.intellij.BundleBase
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 import java.lang.ref.Reference
@@ -12,22 +12,21 @@ import java.util.*
  */
 object ReturnHighlighterBundle {
   private const val BUNDLE = "messages.ReturnHighlighterBundle"
-  private var ourBundle: Reference<ResourceBundle>? = null
+  private var bundle: Reference<ResourceBundle>? = null
 
-  operator fun get(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
-    val bundle = getBundle() ?: return ""
-    return CommonBundle.message(bundle, key, *params)
-  }
+  operator fun get(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String =
+    BundleBase.message(getBundle(), key, *params)
 
   @NonNls
-  private fun getBundle(): ResourceBundle? {
-    var bundle = com.intellij.reference.SoftReference.dereference(ourBundle)
+  private fun getBundle(): ResourceBundle {
+    var derefBundle = com.intellij.reference.SoftReference.dereference(bundle)
 
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE)
-      ourBundle = SoftReference(bundle)
+    if (derefBundle == null) {
+      derefBundle = ResourceBundle.getBundle(BUNDLE)
+      bundle = SoftReference(derefBundle)
+      return derefBundle
     }
 
-    return bundle
+    return derefBundle
   }
 }
